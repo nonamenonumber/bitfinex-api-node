0839cd4881591fd9f1ec6e78caafd0f5f7db5a9d
Louis Hill <louis@louishill.co.uk>
2018-08-25 17:51:26 +0100

    Add fingerprint for symbolDetails

--- index.d.ts
+++ index.d.ts
@@ -269,7 +269,7 @@ declare namespace BFX {
     performance(): void;
     calcAvailableBalance(): void;
     symbols(): void;
-    symbolDetails(): void;
+    symbolDetails(cb: (symbols: any) => any): any;
     accountInfo(): void;
     accountFees(): void;
     accountSummary(): void;
